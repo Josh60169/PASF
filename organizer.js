fd423af6c9b86a6245c3e9b2bf1744532c3c968f
Yash <66393691+Josh60169@users.noreply.github.com>
2025-02-10 14:37:47 +0000

    Added organizer file and started adding foundation for basic organizer functionality.

---
+++ organizer.js
@@ -0,0 +1,40 @@
+//initialize supabase
+const{createClient} = window.supabase;
+const supabaseURL  = "https://weqsojkxxgxhdljikuaa.supabase.co";
+const supabaseAnonKey  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlcXNvamt4eGd4aGRsamlrdWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzEzNTMsImV4cCI6MjAzOTY0NzM1M30.08k0uw8gcUH-VYFHSbQEVlUbXDqTltxmh7COtAEJfIA";
+
+supabase = createClient(supabaseURL, supabaseAnonKey);
+//fetch amd display user data
+
+const profileDataDiv = document.getElementById("profile-data");
+
+let sessions = null;
+
+async function getSession(){
+    sessions = await supabase.auth.getSession();
+    return sessions;
+}
+
+//call the async function
+getSession().then(sessions =>{
+    console.log(sessions);
+}).catch(error =>{console.log('Error fetching session: ', error);
+});
+
+async function getUserProfile(){
+    const {data: userProfile, error} = await supabase.from('table1').select();//'*').eq('id',session.user.id).single();
+
+    if(error){
+        console.log('Error fetching user data: ',error);
+        return null;
+    }
+
+    return userProfile;
+}
+
+const addBtnClicked = () => {
+
+};
+const removeBtnClicked = () => {
+
+};
