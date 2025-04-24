7b9f76d7772a07d5df96d3d76d93fa30aec4084c
Yash <66393691+Josh60169@users.noreply.github.com>
2025-04-24 08:17:55 -0500

    Fixed some bugs

--- studyTimer.js
+++ studyTimer.js
@@ -62,14 +62,14 @@ const countDownTimer = () => {
         setTimer(parseInt(time[0]) - 1, 59, 59)
     } else {
         timerFlag = true;
-        stopBtn.disabled = true;
-        startBtn.disabled = false;
+        // stopBtn.disabled = true;
+        // startBtn.disabled = false;
 
-        if (alarmFlag) {
-            alarmFlag = false;
-            alarmSound.pause();
-            alarmSound.currentTime = 0;
-        }
+        // if (alarmFlag) {
+        //     alarmFlag = false;
+        //     alarmSound.pause();
+        //     alarmSound.currentTime = 0;
+        // }
         timerAlarm();
     }
 };
@@ -77,52 +77,54 @@ const countDownTimer = () => {
 // Sets the time on the timer display
 const setTimer = (hrs, mins, secs) => {
     // adjusts the hrs, mins, and secs to be in proper format (ex: not 90 secs but 1 min and 30 secs)
-    if (Math.trunc(parseInt(mins) / 60) !== 0) {
-        let addToHours = Math.trunc(parseInt(mins) / 60);
-        hrs = parseInt(hrs) + addToHours;
-        mins = parseInt(mins) % 60;
+    if (!alarmFlag) {
+        if (Math.trunc(parseInt(mins) / 60) !== 0) {
+            let addToHours = Math.trunc(parseInt(mins) / 60);
+            hrs = parseInt(hrs) + addToHours;
+            mins = parseInt(mins) % 60;
+        }
+
+        if (Math.trunc(parseInt(secs) / 60) !== 0) {
+            let addToMins = Math.trunc(parseInt(secs) / 60);
+            mins = parseInt(mins) + addToMins;
+            secs = parseInt(secs) % 60;
+        }
+
+        let newHrs = '';
+        let newMins = '';
+        let newSecs = '';
+
+        if (hrs === 0)
+            newHrs = '00';
+        else if (hrs < 10)
+            newHrs = `0${hrs}`;
+        else
+        newHrs = `${hrs}`;
+
+        if (mins === 0)
+            newMins = '00';
+        else if (mins < 10)
+            newMins = `0${mins}`;
+        else
+        newMins = `${mins}`;
+
+        if (secs === 0)
+            newSecs = '00';
+        else if (secs < 10)
+            newSecs = `0${secs}`;
+        else
+            newSecs = `${secs}`;
+
+        timer.innerText = `${newHrs}:${newMins}:${newSecs}`;
     }
-
-    if (Math.trunc(parseInt(secs) / 60) !== 0) {
-        let addToMins = Math.trunc(parseInt(secs) / 60);
-        mins = parseInt(mins) + addToMins;
-        secs = parseInt(secs) % 60;
-    }
-
-    let newHrs = '';
-    let newMins = '';
-    let newSecs = '';
-
-    if (hrs === 0)
-        newHrs = '00';
-    else if (hrs < 10)
-        newHrs = `0${hrs}`;
-    else
-       newHrs = `${hrs}`;
-
-    if (mins === 0)
-        newMins = '00';
-    else if (mins < 10)
-        newMins = `0${mins}`;
-    else
-       newMins = `${mins}`;
-
-    if (secs === 0)
-        newSecs = '00';
-    else if (secs < 10)
-        newSecs = `0${secs}`;
-    else
-        newSecs = `${secs}`;
-
-    timer.innerText = `${newHrs}:${newMins}:${newSecs}`;
 };
 
 const stopTimer = () => {
     if (timerFlag) {
         clearInterval(countDownID);
         clearInterval(stopTimerID);
-        startBtn.disabled = false;
-        stopBtn.disabled = true;
+        // startBtn.disabled = false;
+        // stopBtn.disabled = true;
     }
 };
 
@@ -144,4 +146,5 @@ const timerAlarm = () => {
     alarmSound = new Audio("./assets/audio/alarmSound.wav");
     alarmSound.loop = true;
     alarmSound.play();
+    alert('Time is up!');
 };
